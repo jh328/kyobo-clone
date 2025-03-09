4402e8145fda1f87e735b41894f259dca3936f35
jh328 <wnsgks32810@naver.com>
2025-03-10 00:06:41 +0900

    main title complate

--- src/app/components/main/Main.tsx
+++ src/app/components/main/Main.tsx
@@ -5,7 +5,43 @@ export default function Main() {
         <div className="contents_inner">
             <div className="title_size_xxl">
                 <h2 className="title_heading">오늘의 선택</h2>
-                <ul className="category_tag_list"></ul>
+                <ul className="category_tag_list">
+                    <li className="category_item">
+                        <button className="btn_category">
+                            <span className="text">전체</span>
+                        </button>
+                    </li>
+                    <li className="category_item">
+                        <button className="btn_category_remain">
+                            <span className="text">국내도서</span>
+                        </button>
+                    </li>
+                    <li className="category_item">
+                        <button className="btn_category_remain">
+                            <span className="text">외국도서</span>
+                        </button>
+                    </li>
+                    <li className="category_item">
+                        <button className="btn_category_remain">
+                            <span className="text">eBook</span>
+                        </button>
+                    </li>
+                    <li className="category_item">
+                        <button className="btn_category_remain">
+                            <span className="text">sam</span>
+                        </button>
+                    </li>
+                    <li className="category_item">
+                        <button className="btn_category_remain">
+                            <span className="text">핫트랙스</span>
+                        </button>
+                    </li>
+                    <li className="category_item category_item_right">
+                        <button className="btn_category_remain">
+                            <span className="text">교보ONLY</span>
+                        </button>
+                    </li>
+                </ul>
                 <div className="right_area"></div>
             </div>
             <div className="showcase_template_wrap"></div>
