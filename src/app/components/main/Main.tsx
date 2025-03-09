a646ade1fca52336b0641899e60d1adbfb85b413
jh328 <wnsgks32810@naver.com>
2025-03-09 22:43:53 +0900

    main component

---
+++ src/app/components/main/Main.tsx
@@ -0,0 +1,14 @@
+import "./Main.modules.css"
+
+export default function Main() {
+    return (
+        <div className="contents_inner">
+            <div className="title_size_xxl">
+                <h2 className="title_heading">오늘의 선택</h2>
+                <ul className="category_tag_list"></ul>
+                <div className="right_area"></div>
+            </div>
+            <div className="showcase_template_wrap"></div>
+        </div>
+    )
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Header from "@/app/components/header/Header";
 import Section from "@/app/components/section/Section";
+import Main from "@/app/components/main/Main";
 export default function Home() {
     return (
         <div className="wrapper">
             <Header/>
             <Section/>
+            <Main/>
         </div>
     );
 }
