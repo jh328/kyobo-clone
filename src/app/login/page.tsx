7e28ca66f08e7e7b129c368d6f208764529b1735
jh328 <wnsgks32810@naver.com>
2025-04-11 03:34:39 +0900

    login page component css -> module css updated

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import "./login.modules.css"
+import styles from "./login.module.css"
 import Link from "next/link";
 import {useUserStore} from "@/app/store/userStore";
 import { signIn } from "next-auth/react";
@@ -8,99 +8,99 @@ export default function Page() {
     const {name} = useUserStore();
 
     return (
-        <div className="wrapper member_login">
+        <div className={`${styles.wrapper} ${styles.member_login}`}>
             {name ? (
                 <p>환영합니다, {name}님!</p> // ✅ 로그인된 경우 UI 변경
             ) : (
-                <ul className="sns_login_list">
-                    <li className="sns_login_item sns_item_ml">
+                <ul className={styles.sns_login_list}>
+                    <li className={`${styles.sns_login_item} ${styles.sns_item_ml}`}>
                         {/* ✅ 카카오 로그인 버튼 */}
-                        <button onClick={() => signIn("kakao")} className="btn_sns_login btn_kakao">
-                            <span className="hidden">카카오 로그인</span>
+                        <button onClick={() => signIn("kakao")} className={`${styles.btn_sns_login} ${styles.btn_kakao}`}>
+                            <span className={styles.hidden}>카카오 로그인</span>
                         </button>
                     </li>
                 </ul>
             )}
-            <header className="header_wrapper">
-                <div className="header_inner">
-                    <div className="logo_box">
-                        <Link href="/" className="logo_link">
+            <header className={styles.header_wrapper}>
+                <div className={styles.header_inner}>
+                    <div className={styles.logo_box}>
+                        <Link href="/" className={styles.logo_link}>
                             <img src="/images/common/img_logo_kyobo_footer@2x.png" alt="교보문고 로고"
-                                 className="logo_link_img"/>
+                                 className={styles.logo_link_img}/>
                         </Link>
                     </div>
                 </div>
             </header>
-            <main className="container_wrapper">
-                <section className="contents_wrap login">
-                    <div className="login_form_wrap" style={{borderBottom: "none"}}>
-                        <div className="form_com_group valid_check">
-                            <div className="col_box id">
-                                <div className="awesomplete">
-                                    <input type="email" title="아이디입력" className="form_ip bblr"
+            <main className={styles.container_wrapper}>
+                <section className={`${styles.contents_wrap} ${styles.login}`}>
+                    <div className={styles.login_form_wrap} style={{borderBottom: "none"}}>
+                        <div className={`${styles.form_com_group} ${styles.valid_check}`}>
+                            <div className={`${styles.col_box} ${styles.id}`}>
+                                <div className={styles.awesomplete}>
+                                    <input type="email" title="아이디입력" className={`${styles.form_ip} ${styles.bblr}`}
                                            placeholder="아이디입력해주세요."/>
-                                    <div className="auto_complete_box">
-                                        <div className="custom_scroll_wrap" data-simplebar="init"></div>
+                                    <div className={styles.auto_complete_box}>
+                                        <div className={styles.custom_scroll_wrap} data-simplebar="init"></div>
                                     </div>
-                                    <span className="visually-hidden"></span>
+                                    <span className={styles.visually_hidden}></span>
                                 </div>
                                 <input type="hidden" name="_csrf"/>
-                                <span className="form_desc tip">아이디를 입력해 주세요.</span>
+                                <span className={`${styles.form_desc} ${styles.tip}`}>아이디를 입력해 주세요.</span>
                             </div>
-                            <div className="col_id_pw">
-                                <div className="form_ip_pw">
-                                    <input type="password" className="form_ip bblr" placeholder="비밀번호를 입력해 주세요."
+                            <div className={styles.col_id_pw}>
+                                <div className={styles.form_ip_pw}>
+                                    <input type="password" className={`${styles.form_ip} ${styles.bblr}`} placeholder="비밀번호를 입력해 주세요."
                                            title="비밀번호 입력"/>
-                                    <button className="btn_toggle_pw" type="button">
-                                        <span className="hidden">비밀번호 숨김 상태</span>
+                                    <button className={styles.btn_toggle_pw} type="button">
+                                        <span className={styles.hidden}>비밀번호 숨김 상태</span>
                                     </button>
                                 </div>
                             </div>
-                            <span className="vaild_decs">아이디를 입력해 주세요.</span>
+                            <span className={styles.vaild_decs}>아이디를 입력해 주세요.</span>
                         </div>
-                        <div className="captch_wrap"></div>
-                        <div className="btn_wrap justify">
-                            <button className="btn_lg_a btn_light_gray btn_login">
-                                <span className="text">로그인</span>
+                        <div className={styles.captch_wrap}></div>
+                        <div className={`${styles.btn_wrap_lg} ${styles.justify}`}>
+                            <button className={`${styles.btn_lg_a} ${styles.btn_light_gray} ${styles.btn_login} ${styles.btn_justify}`}>
+                                <span className={styles.text}>로그인</span>
                             </button>
                         </div>
-                        <div className="save_id_box">
-                            <span className="form_chk">
+                        <div className={styles.save_id_box}>
+                            <span className={styles.form_chk}>
                                 <input type="checkbox"/>
-                                <label htmlFor="" className="formSaveId">
+                                <label htmlFor="" className={styles.formSaveId}>
                                     아이디 저장
                                 </label>
                             </span>
-                            <div className="right_area">
+                            <div className={styles.right_area}>
                                 <Link className="" href="">
-                                    <span className="btn_text_id_link">아이디 찾기</span>
+                                    <span className={styles.btn_text_id_link}>아이디 찾기</span>
                                 </Link>
-                                <span className="gap"></span>
+                                <span className={styles.gap}></span>
                                 <Link href="" className="">
-                                    <span className="btn_text_id_link">비밀번호 찾기</span>
+                                    <span className={styles.btn_text_id_link}>비밀번호 찾기</span>
                                 </Link>
                             </div>
                         </div>
-                        <div className="sns_login_box">
-                            <ul className="sns_login_list">
-                                <li className="sns_login_item ">
-                                    <button className="btn_naver btn_sns_lg btn_sns_login">
-                                        <span className="hidden">네이버 로그인</span>
+                        <div className={styles.sns_login_box}>
+                            <ul className={styles.sns_login_list}>
+                                <li className={styles.sns_login_item} >
+                                    <button className={`${styles.btn_naver} ${styles.btn_sns_lg} ${styles.btn_sns_login}`}>
+                                        <span className={styles.hidden}>네이버 로그인</span>
                                     </button>
                                 </li>
-                                <li className="sns_login_item sns_item_ml">
-                                    <button className="btn_kakao btn_sns_lg btn_sns_login">
-                                        <span className="hidden">카카오 로그인</span>
+                                <li className={`${styles.sns_login_item} ${styles.sns_item_ml}`}>
+                                    <button className={`${styles.btn_kakao} ${styles.btn_sns_lg} ${styles.btn_sns_login}`}>
+                                        <span className={styles.hidden}>카카오 로그인</span>
                                     </button>
                                 </li>
-                                <li className="sns_login_item sns_item_ml">
-                                    <button className="btn_goolge btn_sns_lg btn_sns_login">
-                                        <span className="hidden">구글 로그인</span>
+                                <li className={`${styles.sns_login_item} ${styles.sns_item_ml}`}>
+                                    <button className={`${styles.btn_goolge} ${styles.btn_sns_lg} ${styles.btn_sns_login}`}>
+                                        <span className={styles.hidden}>구글 로그인</span>
                                     </button>
                                 </li>
-                                <li className="sns_login_item sns_item_ml">
-                                    <button className="btn_apple btn_sns_lg btn_sns_login">
-                                        <span className="hidden">애플 로그인</span>
+                                <li className={`${styles.sns_login_item} ${styles.sns_item_ml}`}>
+                                    <button className={`${styles.btn_apple} ${styles.btn_sns_lg} ${styles.btn_sns_login}`}>
+                                        <span className={styles.hidden}>애플 로그인</span>
                                     </button>
                                 </li>
                             </ul>
@@ -110,20 +110,20 @@ export default function Page() {
                                 상태를 꼭 확인해 주세요.
                             </p>*/}
                         </div>
-                        <div className="join_induce_wrap">
-                            <div className="login_banner_box hidden">
-                                <Link href="" className="banner_link">
-                                    <span className="hidden">이거 안해도 됐던거임 하</span>
+                        <div className={styles.join_induce_wrap}>
+                            <div className={`${styles.login_banner_box} ${styles.hidden}`}>
+                                <Link href="" className={styles.banner_link}>
+                                    <span className={styles.hidden}>이거 안해도 됐던거임 하</span>
                                 </Link>
                             </div>
-                            <div className="btn_wrap justify">
-                                <Link href="/join" className="btn_lg_join">
-                                    <span className="text">회원가입</span>
+                            <div className={`${styles.btn_wrap} ${styles.justify}`}>
+                                <Link href="/join" className={styles.btn_lg_join}>
+                                    <span className={styles.text}>회원가입</span>
                                 </Link>
                             </div>
-                            <div className="btn_wrap justify">
-                                <Link href="" className="btn_line_gray btn_wrap_Nonmem">
-                                    <span className="text">비회원 주문조회</span>
+                            <div className={`${styles.btn_wrap} ${styles.justify}`}>
+                                <Link href="" className={`${styles.btn_line_gray} ${styles.btn_wrap_Nonmem}`}>
+                                    <span className={styles.text}>비회원 주문조회</span>
                                 </Link>
                             </div>
                         </div>
@@ -131,9 +131,9 @@ export default function Page() {
                     <div className="adv_wrap null 아무 속성 없음. 이건 안해도 되는 것 같음. 다 들어가봐도 화면에 안나옴."></div>
                 </section>
             </main>
-            <footer className="footer_wrapper">
-                <div className="footer_inner">
-                    <div className="copyright">© KYOBO BOOK CENTRE</div>
+            <footer className={styles.footer_wrapper}>
+                <div className={styles.footer_inner}>
+                    <div className={styles.copyright}>© KYOBO BOOK CENTRE</div>
                 </div>
             </footer>
         </div>
